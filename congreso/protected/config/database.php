cdd476caf0affb3cff0571cf94d69d3452029eb3
equipo1 <ruben98.jrgl@gmail.com>
2015-04-16 20:15:00 -0500

    equipo1

---
+++ congreso/protected/config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	
+	'connectionString' => 'mysql:host=localhost;dbname=equipo1',
+	'emulatePrepare' => true,
+	'username' => 'equipo1',
+	'password' => 'equipo1',
+	'charset' => 'utf8',
+	
+);
